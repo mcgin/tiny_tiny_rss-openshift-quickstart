2efbc1c133a1ebe51471d23c973396102d53cc2c
Richard Quirk <richard.quirk@gmail.com>
2013-07-24 19:19:19 +0200

    Run cronjob hourly and every 3 hours, not 15

--- .openshift/cron/hourly/update.sh
+++ .openshift/cron/hourly/update.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 hh=$(date +%H) ; hhn=${hh##0}
-if(($hhn % 15 == 0))
-then
+if [ $(expr $hhn % 3 ) -eq 0 ] ; then
   $OPENSHIFT_REPO_DIR/php/update.php --feeds &> $OPENSHIFT_PHP_LOG_DIR/rss_update.log
 fi
