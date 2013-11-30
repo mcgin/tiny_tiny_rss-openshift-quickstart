b7bfd6dbd83bd04caeae8501302426a5176fbf85
Richard Quirk <richard.quirk@gmail.com>
2013-11-30 10:03:31 +0100

    Run update every hour

--- .openshift/cron/hourly/update.sh
+++ .openshift/cron/hourly/update.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-hh=$(date +%H) ; hhn=${hh##0}
-if [ $(expr $hhn % 3 ) -eq 0 ] ; then
-  $OPENSHIFT_REPO_DIR/php/update.php --feeds &> $OPENSHIFT_PHP_LOG_DIR/rss_update.log
-fi
+$OPENSHIFT_REPO_DIR/php/update.php --feeds &>> $OPENSHIFT_PHP_LOG_DIR/rss_update.log
